030e317908d524e914372f42c10823d4ab265bd0
Sam Wang <wang79922@gmail.com>
2020-05-16 18:47:21 -0700

    Initial Commit

---
+++ friendpoint/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  friendpoint
+//
+//  Created by Sam Wang on 5/16/20.
+//  Copyright © 2020 Sam Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
